f7280c4739840f1d9c41341d7ead2aed6b1bc629
Minh Quy <sugiacupit@gmail.com>
2018-06-29 00:08:20 +0200

    Update Parser for Pascal

--- src/ast.ts
+++ src/ast.ts
@@ -1,13 +1,114 @@
 import { Token } from "./token";
 
-export class TreeNode {
-  left?: TreeNode | Token;
-  operator: Token;
-  right: TreeNode | Token;
+export class Node {}
 
-  constructor({ left, operator, right }: TreeNode) {
+export class ProgramNode extends Node {
+  block: BlockNode;
+  name: string;
+
+  constructor(name: string, block: BlockNode) {
+    super();
+
+    this.name = name;
+    this.block = block;
+  }
+}
+
+export class BlockNode extends Node {
+  declaration: DeclarationNode;
+  compound: CompoundNode;
+
+  constructor(declaration: DeclarationNode, compound: CompoundNode) {
+    super();
+
+    this.declaration = declaration;
+    this.compound = compound;
+  }
+}
+
+export class DeclarationNode extends Node {
+  children: VariableDeclarationNode[];
+
+  constructor(children: VariableDeclarationNode[]) {
+    super();
+
+    this.children = children;
+  }
+}
+
+export class VariableDeclarationNode extends Node {
+  name: TokenNode;
+  type: TokenNode;
+
+  constructor(name: TokenNode, type: TokenNode) {
+    super();
+
+    this.name = name;
+    this.type = type;
+  }
+}
+
+export class TokenNode extends Node {
+  token: Token;
+
+  constructor(token: Token) {
+    super();
+
+    this.token = token;
+  }
+}
+
+export class CompoundNode extends Node {
+  children: (CompoundNode | AssignmentNode)[];
+
+  constructor(children: (CompoundNode | AssignmentNode)[]) {
+    super();
+
+    this.children = children;
+  }
+}
+
+export class AssignmentNode extends Node {
+  variable: TokenNode;
+  expression: ExpressionNode | UnaryNode;
+
+  constructor(variable: TokenNode, expression: ExpressionNode | UnaryNode) {
+    super();
+
+    this.variable = variable;
+    this.expression = expression;
+  }
+}
+
+export class ExpressionNode extends Node {
+  left: ExpressionNode | UnaryNode | TokenNode;
+  operator: Token;
+  right: ExpressionNode | UnaryNode | TokenNode;
+
+  constructor(
+    left: ExpressionNode | UnaryNode | TokenNode,
+    operator: Token,
+    right: ExpressionNode | UnaryNode | TokenNode
+  ) {
+    super();
+
     this.left = left;
     this.operator = operator;
     this.right = right;
+  }
+}
+
+export class UnaryNode extends Node {
+  operator: Token;
+  operand: ExpressionNode | UnaryNode | TokenNode;
+
+  constructor(
+    operator: Token,
+    operand: ExpressionNode | UnaryNode | TokenNode
+  ) {
+    super();
+
+    this.operator = operator;
+    this.operand = operand;
   }
 }
--- src/parser.ts
+++ src/parser.ts
@@ -1,20 +1,19 @@
 import { TType, Token } from "./token";
-import { TreeNode } from "./ast";
+import * as AST from "./ast";
 
 /**
  * program : PROGRAM variable SEMI block dot
- * block : declarations compound_statement
- * declarations : VAR (variable_declaration SEMI)+ | empty
+ * block : declaration compound_statement
+ * declaration : VAR (variable_declaration SEMI)+ | empty
  * variable_declaration : variable (COMMA ID)* COLON typespec
  * typespec : INTEGER | REAL
- * compound_statement : BEGIN statement_list END
- * statement_list : statement (SEMI statement)*
- * statement : compound_statement | assignment_statement | empty
- * assignment_statement : variable ASSIGN expr
+ * compound_statement : BEGIN statement (SEMI statement)* END
+ * statement : compound_statement | assignment_statement
+ * assignment_statement : variable ASSIGN expression
  * empty :
- * expr : term ((PLUS | MINUS) term)*
+ * expression : term ((PLUS | MINUS) term)*
  * term : factor ((MUL | DIV) factor)*
- * factor : (PLUS | MINUS) expr | INTEGER_CONST | REAL_CONST | LPAREN expr RPAREN | variable
+ * factor : (PLUS | MINUS) expression | INTEGER_CONST | REAL_CONST | LPAREN expression RPAREN | variable
  * variable : ID
  */
 
@@ -31,65 +30,166 @@ export class Parser {
     return this.expression();
   }
 
-  expression(): TreeNode {
-    let node = this.factor();
-    let token = this.currentToken;
+  eat(type: TType) {
+    let token = this.tokens[this.cPointer];
+
+    if (token.type === type) {
+      this.cPointer += 1;
+      return token;
+    } else {
+      throw new Error("Wrong syntax");
+    }
+  }
+
+  program(): AST.ProgramNode {
+    this.eat(TType.PROGRAM);
+    let variableNode = this.variable();
+    this.eat(TType.SEMI);
+    let blockNode = this.block();
+    this.eat(TType.DOT);
+    return new AST.ProgramNode(variableNode.token.value.toString(), blockNode);
+  }
+
+  variable(): AST.TokenNode {
+    let token = this.eat(TType.VARIABLE_NAME);
+
+    return new AST.TokenNode(token);
+  }
+
+  block(): AST.BlockNode {
+    let declarationNode = this.declaration();
+    let compoundNode = this.compound();
+
+    return new AST.BlockNode(declarationNode, compoundNode);
+  }
+
+  declaration(): AST.DeclarationNode {
+    let declarations: AST.VariableDeclarationNode[] = [];
+
+    if (this.getCurrentToken().type === TType.VAR) {
+      this.eat(TType.VAR);
+      while (this.getCurrentToken().type === TType.VARIABLE_NAME) {
+        let nodes = this.variableDeclaration();
+        declarations.push(...nodes);
+        this.eat(TType.SEMI);
+      }
+    }
+    return new AST.DeclarationNode(declarations);
+  }
+
+  variableDeclaration(): AST.VariableDeclarationNode[] {
+    let variableName = this.eat(TType.VARIABLE_NAME);
+    let declarations = [new AST.TokenNode(variableName)];
+
+    while (this.getCurrentToken().type === TType.COMMA) {
+      this.eat(TType.COMMA);
+      variableName = this.eat(TType.VARIABLE_NAME);
+      declarations.push(new AST.TokenNode(variableName));
+    }
+    this.eat(TType.COLON);
+    let type = this.type();
+    return declarations.map(
+      declaration => new AST.VariableDeclarationNode(declaration, type)
+    );
+  }
+
+  type(): AST.TokenNode {
+    let token = this.getCurrentToken();
+
+    if (token.type === TType.INTEGER) {
+      return new AST.TokenNode(this.eat(TType.INTEGER));
+    } else if (token.type === TType.REAL) {
+      return new AST.TokenNode(this.eat(TType.REAL));
+    } else {
+      throw new Error("Wrong syntax");
+    }
+  }
+
+  compound(): AST.CompoundNode {
+    this.eat(TType.BEGIN);
+    let statement = this.statement();
+    let statements = [statement];
+
+    while (this.getCurrentToken().type === TType.SEMI) {
+      this.eat(TType.SEMI);
+      statement = this.statement();
+      statements.push(statement);
+    }
+    return new AST.CompoundNode(statements);
+  }
+
+  statement(): AST.CompoundNode | AST.AssignmentNode {
+    let token = this.getCurrentToken();
+
+    if (token.type === TType.BEGIN) {
+      return this.compound();
+    } else if (token.type === TType.VARIABLE_NAME) {
+      return this.assignment();
+    } else {
+      throw new Error("Wrong syntax");
+    }
+  }
+
+  assignment(): AST.AssignmentNode {
+    let variable = this.eat(TType.VARIABLE_NAME);
+    this.eat(TType.ASSIGN);
+    let expression = this.expression();
+
+    return new AST.AssignmentNode(new AST.TokenNode(variable), expression);
+  }
+
+  expression(): AST.ExpressionNode {
+    let node = this.term();
+    let token = this.getCurrentToken();
 
     while (token && (token.type === TType.PLUS || token.type === TType.MINUS)) {
       this.cPointer += 1;
-      node = new TreeNode({
-        left: node,
-        operator: token,
-        right: this.factor()
-      });
-      token = this.currentToken;
+      node = new AST.ExpressionNode(node, token, this.term());
+      token = this.getCurrentToken();
     }
 
     return node;
   }
 
-  factor(): TreeNode {
+  term(): AST.ExpressionNode {
     let node = this.term();
-    let token = this.currentToken;
+    let token = this.getCurrentToken();
 
     while (token && (token.type === TType.MUL || token.type === TType.DIV)) {
       this.cPointer += 1;
-      node = new TreeNode({
-        left: node,
-        operator: token,
-        right: this.factor()
-      });
-      token = this.currentToken;
+      node = new AST.ExpressionNode(node, token, this.factor());
+      token = this.getCurrentToken();
     }
 
     return node;
   }
 
-  term(): TreeNode {
-    let token = this.currentToken;
+  factor(): AST.ExpressionNode | AST.UnaryNode | AST.TokenNode {
+    let token = this.getCurrentToken();
 
-    if (token.type === TType.OPEN_PAREN) {
-      this.cPointer += 1;
+    if (token.type === TType.LPAREN) {
+      this.eat(TType.LPAREN);
       const node = this.expression();
-      this.cPointer += 1;
+      this.eat(TType.RPAREN);
       return node;
-    } else if (token.type === TType.PLUS || token.type === TType.MINUS) {
-      this.cPointer += 1;
-      return new TreeNode({ operator: token, right: this.expression() });
+    } else if (token.type === TType.PLUS) {
+      this.eat(TType.PLUS);
+      return new AST.UnaryNode(token, this.expression());
+    } else if (token.type === TType.MINUS) {
+      this.eat(TType.MINUS);
+      return new AST.UnaryNode(token, this.expression());
+    } else if (token.type === TType.REAL_CONST) {
+      this.eat(TType.REAL_CONST);
+      return new AST.TokenNode(token);
+    } else if (token.type === TType.INTEGER_CONST) {
+      this.eat(TType.INTEGER_CONST);
+      return new AST.TokenNode(token);
     } else {
-      this.cPointer += 1;
-      return new TreeNode({
-        operator: { type: TType.PLUS, value: "+" },
-        right: token
-      });
+      return this.variable();
     }
   }
 
-  get currentToken() {
+  getCurrentToken() {
     return this.tokens[this.cPointer];
-  }
-
-  get nextToken() {
-    return this.tokens[this.cPointer + 1];
   }
 }
