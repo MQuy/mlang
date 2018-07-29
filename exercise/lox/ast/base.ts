d68e0af302886f0f557165e122f739e755a5f432
Minh Quy <sugiacupit@gmail.com>
2018-07-29 22:22:41 +0200

    Remove exercise

--- exercise/lox/ast/base.ts
+++
@@ -1,9 +0,0 @@
-export abstract class AstNode {}
-
-export class ReturnError {
-  value: any;
-
-  constructor(value: any) {
-    this.value = value;
-  }
-}
--- exercise/lox/index.ts
+++
@@ -1,6 +0,0 @@
-import { Lexer } from "./lexer";
-import { Parser } from "./parser";
-import { Interpreter } from "./interpreter";
-import { Resolver } from "./resolver";
-
-export { Lexer, Parser, Interpreter, Resolver };
